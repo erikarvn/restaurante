a0fdcf9b9a4dba8e935d89f94c52f8d5e21b85ab
irmp <hola>
2014-10-11 10:47:35 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Restaurente::Application.config.secret_key_base = 'd38e755ab00817ddecbdc83efd3ac642ab79bab7da69c799eaf93e45cb9cad34ca9894da61d9f0c8579b6d713ad5e1f82a498a4d75dbb1fcb0ca9a6a4df3256a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Restaurente::Application.config.session_store :cookie_store, key: '_restaurente_session'
